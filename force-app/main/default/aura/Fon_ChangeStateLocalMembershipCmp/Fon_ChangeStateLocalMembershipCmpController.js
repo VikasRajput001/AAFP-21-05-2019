abc89c81b1d6b98bb60e81bb71b9419869db93af
losindia <vikasrajput3601@gmail.com>
2019-06-05 20:39:40 +0530

    New Update Done At 24-05-2019 Version 2

--- force-app/main/default/aura/Fon_ChangeStateLocalMembershipCmp/Fon_ChangeStateLocalMembershipCmpController.js
+++ force-app/main/default/aura/Fon_ChangeStateLocalMembershipCmp/Fon_ChangeStateLocalMembershipCmpController.js
@@ -9,8 +9,7 @@
     recordSubmitted : function(component, event, helper){
         event.preventDefault();       // stop the form from submitting
         var fields = event.getParam('fields');
-        
-        var action = component.get("c.updateRecords");
+        var action = component.get("c.updateRecords");     
         action.setParams({
             contactId : component.get("v.contactId"),
             activeMembershipId : component.get("v.activeMembershipId"),
@@ -20,7 +19,14 @@
         action.setCallback(this,function(result){
             if(result.getState() == 'SUCCESS'){
                 if(result.getReturnValue()){
-    
+                    component.find('membershipStateLocalForm').submit(fields);
+                }
+                else{
+                    var urlEvent = $A.get("e.force:navigateToURL");
+                    urlEvent.setParams({
+                        "url": "/lightning/r/Contact/"+component.get("v.contactId")+"/view?0.source=alohaHeader"
+                    });
+                    urlEvent.fire();
                 }
             }
             else {
@@ -28,9 +34,17 @@
             }
         });
         $A.enqueueAction(action);
-        
-        //component.find('membershipStateLocalForm').submit(fields);
-        
+    },
+    // this function automatic call by aura:waiting event  
+    showSpinner: function(component, event, helper) {
+        // make Spinner attribute true for display loading spinner 
+        component.set("v.Spinner", true); 
+    },
+    
+    // this function automatic call by aura:doneWaiting event 
+    hideSpinner : function(component,event,helper){
+        // make Spinner attribute to false for hide loading spinner    
+        component.set("v.Spinner", false);
     },
     actionOnSuccess : function(component, event, helper){
         helper.showToastMessage("success", "Success!", "The Membership State/Local has been changed successfully.");
